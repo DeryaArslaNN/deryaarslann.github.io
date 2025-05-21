58cd44e4cea62da29657e8ff5d6dc32e33076aa0
DeryaArslaNN <arslanderya321@gmail.com>
2025-05-21 16:24:47 +0300

    Delete myscript.js

--- myscript.js
+++
@@ -1,5 +0,0 @@
-function rastgeleYemek() {
-  const yemekler = ['Mercimek Çorbası', 'Karnıyarık', 'Tavuk Sote', 'Fırın Makarna', 'Revani'];
-  const secilen = yemekler[Math.floor(Math.random() * yemekler.length)];
-  document.getElementById('yemekSonuc').innerText = "Bugün: " + secilen + " yiyin!";
-}
