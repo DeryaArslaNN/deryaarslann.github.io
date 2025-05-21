5ba038b4693dfedc067c219b4d19069368290cce
DeryaArslaNN <arslanderya321@gmail.com>
2025-05-21 19:42:40 +0300

    Delete myscript.js

--- myscript.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener("DOMContentLoaded", function () {
-    const params = new URLSearchParams(window.location.search);
-    const isim = params.get("isim");
-    const cevap = params.get("cevap");
-    if (isim && cevap) {
-        document.getElementById("sonuclar").innerHTML = `<p><strong>${isim}</strong> şöyle dedi: ${cevap}</p>`;
-    }
-});
