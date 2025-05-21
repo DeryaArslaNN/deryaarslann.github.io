a6e3e6131186dad6802c20bcb7dab44f7dc00e93
DeryaArslaNN <arslanderya321@gmail.com>
2025-05-21 15:58:54 +0300

    Delete myscript.js

--- myscript.js
+++
@@ -1,6 +0,0 @@
-// JS Uygulama: Rastgele Yemek Önerisi
-function rastgeleYemek() {
-  const yemekler = ['Mercimek Çorbası', 'Karnıyarık', 'Tavuk Sote', 'Fırın Makarna', 'Revani'];
-  const secilen = yemekler[Math.floor(Math.random() * yemekler.length)];
-  document.getElementById('yemekSonuc').innerText = "Bugün: " + secilen + " yiyin!";
-}
